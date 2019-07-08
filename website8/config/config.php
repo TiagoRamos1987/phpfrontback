3ba9d370ff14f0bd30984d29cc47f0b40e165e1d
Tiago Ramos <tiagoramos2905@hotmail.com>
2019-07-08 18:13:40 -0300

    first commit

---
+++ website8/config/config.php
@@ -0,0 +1,6 @@
+<?php
+	define('ROOT_URL', 'http://localhost:81/phpfrontback/phpsandbox/website8/');
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PASS', '5sf6$c#@');
+	define('DB_NAME', 'phpblog');
